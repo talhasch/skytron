0da405e77936f5d0a988d901a53319df1fa33db4
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-08-15 20:15:20 +0300

    checkpoint

---
+++ app/components/popover-confirm/index.tsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+
+import { Popover, OverlayTrigger, Button } from 'react-bootstrap';
+
+interface Props {
+  children: JSX.Element;
+  onConfirm?: () => void;
+  onCancel?: () => void;
+}
+
+interface State {
+  show: boolean;
+}
+
+export default class PopoverConfirm extends Component<Props> {
+  state: State = {
+    show: false
+  };
+
+  toggle = (e?: React.MouseEvent) => {
+    if (e) {
+      e.stopPropagation();
+    }
+
+    const { show } = this.state;
+    this.setState({ show: !show });
+  };
+
+  confirm = () => {
+    this.toggle();
+
+    const { onConfirm } = this.props;
+    if (onConfirm) {
+      onConfirm();
+    }
+  };
+
+  cancel = () => {
+    this.toggle();
+
+    const { onCancel } = this.props;
+    if (onCancel) {
+      onCancel();
+    }
+  };
+
+  render() {
+    const { children } = this.props;
+    const { show } = this.state;
+
+    const clonedChildren = React.cloneElement(children, {
+      onClick: this.toggle
+    });
+
+    const popover = (
+      <Popover
+        id="popover-confirm"
+        onClick={(e) => {
+          // Prevent to trigger hide event on modal dialog
+          e.stopPropagation();
+        }}
+      >
+        <Popover.Title>Are you sure?</Popover.Title>
+        <Popover.Content>
+          <div style={{ textAlign: 'center' }}>
+            <Button size="sm" variant="primary" style={{ marginRight: '10px' }} onClick={this.confirm}>
+              OK
+            </Button>
+            <Button size="sm" variant="secondary" onClick={this.cancel}>
+              Cancel
+            </Button>
+          </div>
+        </Popover.Content>
+      </Popover>
+    );
+
+    if (!show) {
+      return clonedChildren;
+    }
+
+    return (
+      <>
+        <OverlayTrigger defaultShow={true} trigger={[]} placement="bottom" overlay={popover}>
+          {children}
+        </OverlayTrigger>
+      </>
+    );
+  }
+}
--- app/components/upload-history/index.tsx
+++ app/components/upload-history/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import { Modal } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 
 import filename from '../../util/filename';
+
 import link from '../../helper/link';
 
 import { HistoryItem } from '../../store/queue/types';
 
 import * as ls from '../../util/local-storage';
+
 import BtnCopy from '../btn-copy';
+import PopoverConfirm from '../popover-confirm';
 
 interface Props {
   onHide: () => void
@@ -23,22 +26,34 @@ export default class UploadHistory extends React.Component<Props, State> {
     data: []
   };
 
-  componentDidMount() {
+  fetch = () => {
     const data: HistoryItem[] = ls.getByPrefix('history')
       .sort((a, b) => b.timestamp - a.timestamp) as HistoryItem[];
 
     this.setState({ data });
+  };
+
+  componentDidMount() {
+    this.fetch();
   }
+
+  clear = () => {
+    ls.removeByPrefix('history');
+    this.fetch();
+  };
 
   render() {
     const { onHide } = this.props;
     const { data } = this.state;
 
-    console.log(data);
-
     return <Modal show={true} size="lg" onHide={onHide} centered={true} className="upload-history-modal">
       <Modal.Header closeButton={true}>
         <Modal.Title>Upload History</Modal.Title>
+        {data.length > 0 && (
+          <PopoverConfirm onConfirm={this.clear}>
+            <Button variant="outline-primary" size="sm">Clear History</Button>
+          </PopoverConfirm>
+        )}
       </Modal.Header>
       <Modal.Body>
         {(() => {
@@ -46,18 +61,19 @@ export default class UploadHistory extends React.Component<Props, State> {
             return <span className="text-muted">History is empty.</span>;
           }
 
-          return data.map(i => (
-            <div className="item">
-              <div className="item-info">
-                <div className="filename">{filename(i.path)}</div>
-                <div className="link"><a href={link(i.link)}>{link(i.link)}</a></div>
+          return <>
+            {data.map((item, i) => (
+              <div className="item" key={i}>
+                <div className="item-info">
+                  <div className="filename">{filename(item.path)}</div>
+                  <div className="link"><a href={link(item.link)}>{link(item.link)}</a></div>
+                </div>
+                <div className="copy">
+                  <BtnCopy value={link(item.link)}/>
+                </div>
               </div>
-              <div className="copy">
-                <BtnCopy value={link(i.link)}/>
-              </div>
-            </div>
-          ));
-
+            ))}
+          </>;
         })()}
       </Modal.Body>
     </Modal>;
